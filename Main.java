857dfac7f9e2d48fad64f08a7598561e54a9071d
Bartłomiej Łysik <100691680+Bartek2134@users.noreply.github.com>
2022-03-15 11:37:37 +0000

    Hello world

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("czesc");
+    System.out.println("Hello");
+     System.out.println("World");
+     System.out.println("!!");
   }
 }
